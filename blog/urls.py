6bd7ffd5f50bdc7dc8097854a336dacb18873e93
nohji <nogood1014@naver.com>
2024-03-06 20:21:25 +0900

    Added post button and delete function.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+
 ]
