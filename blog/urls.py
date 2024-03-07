d2d18e7b75b9721bc75f3cf8bef6c874c1290873
nohji <nogood1014@naver.com>
2024-03-07 17:01:52 +0900

    Added comment function.

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    path(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    path(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 
 ]
